68a015ee1cf2255db0c04a873e3577926c85e65a
Alexandre <ale.devx@gmail.com>
2025-09-02 16:34:41 -0400

    feat: added bcrypt net for password encryption

---
+++ src/Backend/Evolutio.Infrastructure/Security/Cryptography/BCryptNet.cs
@@ -0,0 +1,15 @@
+﻿using Evolutio.Domain.Security.Cryptography;
+
+namespace Evolutio.Infrastructure.Security.Cryptography;
+public class BCryptNet : IPasswordEncripter
+{
+    public string Encrypt(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password);
+    }
+    public bool IsValid(string password, string passwordHash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+    }
+}
+
