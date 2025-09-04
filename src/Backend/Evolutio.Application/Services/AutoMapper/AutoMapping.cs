7deb22cc58247eefbf57a1512ec50e382ee3f00b
Alexandre <ale.devx@gmail.com>
2025-09-04 15:24:59 -0400

    feat: added auto mapping class for mapping entities and responses

---
+++ src/Backend/Evolutio.Application/Services/AutoMapper/AutoMapping.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Evolutio.Communication.Requests;
+using Evolutio.Communication.Responses;
+using Sqids;
+
+namespace Evolutio.Application.Services.AutoMapper;
+public class AutoMapping : Profile
+{
+    private readonly SqidsEncoder<long> _idEncoder;
+    public AutoMapping(SqidsEncoder<long> idEncoder)
+    {
+        _idEncoder = idEncoder;
+        RequestToDomain();
+        DomainToResponse();
+    }
+    // Requests -> Domínio
+    private void RequestToDomain()
+    {
+        CreateMap<RequestRegisterUserJson, Domain.Entities.User>()
+        .ForMember(dest => dest.Password, opt => opt.Ignore());
+    }
+    // Domínio -> Responses
+    private void DomainToResponse()
+    {
+        CreateMap<Domain.Entities.User, ResponseUserProfileJson>();
+    }
+}
+
