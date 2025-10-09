6da65119b0273496141052ca0f68b475911aa2a6
Alexandre <ale.devx@gmail.com>
2025-10-09 18:44:54 -0400

    refactor: added perfil role on user

--- src/Backend/Evolutio.Domain/Evolutio.Domain/Entities/User.cs
+++ src/Backend/Evolutio.Domain/Evolutio.Domain/Entities/User.cs
@@ -1,10 +1,13 @@
-﻿namespace Evolutio.Domain.Entities;
+﻿using Evolutio.Domain.Enums;
+
+namespace Evolutio.Domain.Entities;
 
 public class User : EntityBase
 {
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    public Perfil Perfil { get; set; } = Perfil.Funcionario;
     public Guid UserIdentifier { get; set; } = Guid.NewGuid();
 }
 
---
+++ src/Backend/Evolutio.Domain/Evolutio.Domain/Enums/Perfil.cs
@@ -0,0 +1,7 @@
+﻿namespace Evolutio.Domain.Enums;
+public enum Perfil
+{
+    Admin = 0,
+    Funcionario = 1,
+}
+
