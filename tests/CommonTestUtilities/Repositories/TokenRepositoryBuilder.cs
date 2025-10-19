24127069637fb39c25c5ee85cdb7426eeffde963
Alexandre <ale.devx@gmail.com>
2025-10-19 16:49:43 -0400

    feat: added token utilities

---
+++ tests/CommonTestUtilities/Repositories/TokenRepositoryBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Evolutio.Domain.Entities;
+using Evolutio.Domain.Repositories.Token;
+using Moq;
+
+namespace CommonTestUtilities.Repositories;
+public class TokenRepositoryBuilder
+{
+    private readonly Mock<ITokenRepository> _repository;
+    public TokenRepositoryBuilder()
+    {
+        _repository = new Mock<ITokenRepository>();
+    }
+    public TokenRepositoryBuilder Get(RefreshToken refreshToken)
+    {
+        _repository.Setup(repository => repository.Get(refreshToken.Value)).ReturnsAsync(refreshToken);
+
+        return this;
+    }
+    public void SaveNewRefreshToken(RefreshToken refreshToken)
+    {
+        _repository.Setup(repository => repository.SaveNewRefreshToken(refreshToken)).Returns(Task.CompletedTask);
+    }
+    public ITokenRepository Build() 
+    {
+        return _repository.Object;
+    }
+}
---
+++ tests/CommonTestUtilities/Tokens/RefreshTokenGeneratorBuilder.cs
@@ -0,0 +1,8 @@
+﻿using Evolutio.Domain.Security.Tokens;
+using Evolutio.Infrastructure.Security.Tokens.Refresh;
+
+namespace CommonTestUtilities.Tokens;
+public class RefreshTokenGeneratorBuilder
+{
+    public static IRefreshTokenGenerator Build() => new RefreshTokenGenerator();
+}
