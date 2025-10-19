f9787c26648079f658bc2ba9a15e90ef32c6d3d7
Alexandre <ale.devx@gmail.com>
2025-10-19 16:31:48 -0400

    refactor: added user profile for access control and permissions

--- src/Backend/Evolutio.Domain/Evolutio.Domain/Entities/User.cs
+++ src/Backend/Evolutio.Domain/Evolutio.Domain/Entities/User.cs
@@ -1,4 +1,4 @@
-﻿using Evolutio.Domain.Enums;
+﻿using Evolutio.Domain.Constants;
 
 namespace Evolutio.Domain.Entities;
 
@@ -7,7 +7,7 @@ public class User : EntityBase
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public Perfil Perfil { get; set; } = Perfil.Funcionario;
+    public string Perfil { get; set; } = string.Empty;
     public Guid UserIdentifier { get; set; } = Guid.NewGuid();
 }
 
--- src/Backend/Evolutio.Domain/Evolutio.Domain/Enums/Perfil.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Evolutio.Domain.Enums;
-public enum Perfil
-{
-    Admin = 0,
-    Funcionario = 1,
-}
-
