e5f62e356c1c6c0e58b659496bfca79160f1ea63
Alexandre <ale.devx@gmail.com>
2025-10-21 17:27:05 -0400

    feat: added login endpoint

---
+++ src/Backend/Evolutio.API/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using Evolutio.Application.UseCases.Login.DoLogin;
+using Evolutio.Communication.Requests;
+using Evolutio.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Evolutio.API.Controllers;
+public class LoginController : EvolutioBaseController
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Login([FromBody] RequestLoginJson request, [FromServices] IDoLoginUseCase useCase)
+    {
+        var response = await useCase.Execute(request);
+
+        return Ok(response);
+    }
+}
