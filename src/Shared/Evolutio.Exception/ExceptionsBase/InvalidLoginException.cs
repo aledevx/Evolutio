b28471077f57f72f5b10e65cb15350ee843433bf
Alexandre <ale.devx@gmail.com>
2025-10-21 17:17:51 -0400

    feat: added new exception for invalid login

---
+++ src/Shared/Evolutio.Exception/ExceptionsBase/InvalidLoginException.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+
+namespace Evolutio.Exception.ExceptionsBase;
+public class InvalidLoginException : EvolutioException
+{
+    public InvalidLoginException() : base(ResourceMessagesException.EMAIL_OR_PASSWORD_INVALID)
+    {
+    }
+    public override IList<string> GetErrorMessages()
+    {
+        return [Message];
+    }
+
+    public override HttpStatusCode GetStatusCode()
+    {
+        return HttpStatusCode.Unauthorized;
+    }
+}
