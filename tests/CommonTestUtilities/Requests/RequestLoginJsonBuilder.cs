f579e2f3a4fbb5f72eb27e8cc5abad02ff8e66c2
Alexandre <ale.devx@gmail.com>
2025-10-21 17:31:05 -0400

    feat: added test for login use case and create request login builder

---
+++ tests/CommonTestUtilities/Requests/RequestLoginJsonBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Bogus;
+using Evolutio.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+public class RequestLoginJsonBuilder
+{
+    public static RequestLoginJson Build()
+    {
+        var requestGenerated = new Faker<RequestLoginJson>()
+            .RuleFor(request => request.Email, (f) => f.Internet.Email())
+            .RuleFor(request => request.Password, (f) => f.Internet.Password());
+
+        return requestGenerated;
+    }
+}
