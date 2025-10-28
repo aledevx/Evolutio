c63586cbe28a59f4de30d3e6b2d3426d4f2d54dd
Alexandre <ale.devx@gmail.com>
2025-10-28 17:44:22 -0400

    feat: added attribute for authenticated users, creating restriction only for logged in users

---
+++ src/Backend/Evolutio.API/Attributes/AuthenticatedUserAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Evolutio.API.Filters;
+using Evolutio.Communication.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Evolutio.API.Attributes;
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AuthenticatedUserAttribute : TypeFilterAttribute
+{
+    public AuthenticatedUserAttribute(params Perfil[] roles) : base(typeof(AuthenticadedUserFilter))
+    {
+        Arguments = new object[] { roles ?? Array.Empty<Perfil>() };
+    }
+}
