ff61ce30f826b3fce5c290de3cc77a3148519604
Alexandre <ale.devx@gmail.com>
2025-10-28 18:18:23 -0400

    feat: test for use case and endpoint to get user logged profile

---
+++ tests/CommonTestUtilities/LoggedUser/LoggedUserBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Evolutio.Domain.Entities;
+using Evolutio.Domain.Services.LoggedUser;
+using Moq;
+
+namespace CommonTestUtilities.LoggedUser;
+public class LoggedUserBuilder
+{
+    public static ILoggedUser Build(User user)
+    {
+        var mock = new Mock<ILoggedUser>();
+
+        mock.Setup(x => x.User()).ReturnsAsync(user);
+
+        return mock.Object;
+    }
+}
