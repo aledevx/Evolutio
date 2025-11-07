6a8f47e87b4c0081e2c23d7b12431d642989ce73
Alexandre <ale.devx@gmail.com>
2025-11-07 16:30:34 -0400

    feat: added blazor wasm project with mudblazor

---
+++ src/Frontend/Evolutio.Web/Pages/Login/LoginPage.cs
@@ -0,0 +1,33 @@
+﻿using Evolutio.Communication.Requests;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Evolutio.Web.Pages.Login;
+public class LoginPage : ComponentBase
+{
+    #region Properties
+    public bool IsBusy { get; set; } = false;
+    public RequestLoginJson InputModel { get; set; } = new();
+
+    #endregion
+
+    #region Services
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = null!;
+    [Inject]
+    public ISnackbar Snackbar { get; set; } = null!;
+
+    #endregion
+
+    #region Methods
+
+    public void OnValidSubmitAsync()
+    {
+        IsBusy = true;
+
+        Snackbar.Add("Submit ok");
+    }
+
+    #endregion
+}
